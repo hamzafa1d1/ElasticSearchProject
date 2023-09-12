2db11e893be2e87434f9838dede4646bfb506d7e
Hamza Faidi <Hamza.Faidi@expensya.com>
2023-09-12 20:57:29 +0100

    added server files

---
+++ Server/src/main/java/com/ElasticSerachApp/ElasticSerachApp/ElasticSerachAppApplication.java
@@ -0,0 +1,13 @@
+package com.ElasticSerachApp.ElasticSerachApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticSerachAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticSerachAppApplication.class, args);
+	}
+
+}
---
+++ Server/src/test/java/com/ElasticSerachApp/ElasticSerachApp/ElasticSerachAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ElasticSerachApp.ElasticSerachApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticSerachAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
