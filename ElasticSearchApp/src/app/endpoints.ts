a8fc7b3e38cccfe34050742e866b8c62caa248fb
hamzafa1d1 <hamza.faidi@supcom.tn>
2023-11-09 18:20:06 +0100

    rectifying failures in spring boot

--- ElasticSearchApp/src/app/endpoints.ts
+++ ElasticSearchApp/src/app/endpoints.ts
@@ -1,5 +1,5 @@
 export class endpoints {
-  static ServerUrl = "http://localhost:8080/ElasticSearchApp"
+  static ServerUrl = "http://localhost:4444/ElasticSearchApp"
   static GetImages = "/pictures"
   static GetImagesWhenSearchingByPicture='/pictures/searchByPicture'
   static ImageUploadUrl = "/uploaded-image"
